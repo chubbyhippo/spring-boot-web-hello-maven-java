69af7aada174a2126b580a52849c939ec844a715
ch <mkmsacc@outlook.com>
2025-03-05 09:27:27 +0700

    Add HelloController and test for /hello endpoint
    Introduce a `HelloController` with a `/hello` endpoint that returns "Hello". Add integration test to verify the endpoint's behavior using `MockMvcTester`.

--- src/test/java/io/github/chubbyhippo/hello/HelloApplicationTests.java
+++ src/test/java/io/github/chubbyhippo/hello/HelloApplicationTests.java
@@ -1,13 +1,27 @@
 package io.github.chubbyhippo.hello;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.assertj.MockMvcTester;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
 class HelloApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private MockMvcTester mockMvcTester;
+
+    @Test
+    @DisplayName("should return hello")
+    void shouldReturnHello() {
+        mockMvcTester.get()
+                .uri("/hello")
+                .assertThat()
+                .hasStatusOk()
+                .hasBodyTextEqualTo("Hello");
+    }
 
 }
